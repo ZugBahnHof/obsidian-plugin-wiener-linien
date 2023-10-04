3af33e344f988524c72e22d0f9526275e127280a
Julian Leucker <leuckerj@gmail.com>
2023-10-04 13:38:09 +0200

    :lipstick: Display time since last refresh

--- components/Main.svelte
+++ components/Main.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
 	import Train from "./Train.svelte";
 	import {getDepartures} from "../scripts/wienerlinien";
+	import {onDestroy, onMount} from "svelte";
+	import {moment} from "obsidian";
+
 
 	export let rblNumber;
 	export let showRelatedLines = true;
 
 	let departures = {monitors: []};
 	let monitors = [];
+	let lastRefresh: moment;
+	let lastRefreshReadable: string;
 
 	async function loadDepartures() {
 		departures.monitors = (await getDepartures(rblNumber, showRelatedLines))?.monitors;
+		lastRefresh = moment();
+
 		console.log(departures);
 	}
+
+	onMount(loadDepartures)
 
 	$: (monitors = departures.monitors.reduce((acc, obj) => {
 		const title = obj.locationStop.properties.title;
@@ -25,12 +34,30 @@
 
 		return acc;
 	}, []));
+
+	function updateTime (newTime?: moment){
+		lastRefreshReadable = lastRefresh?.fromNow()
+		console.debug("Updating time")
+	}
+
+	$: updateTime(lastRefresh)
+
+  	let interval = setInterval(() => updateTime(), 30000);
+
+	onDestroy(() => {
+		clearInterval(interval);
+		console.debug("Interval cleared");
+	});
 </script>
 
 <div class="header">
 	<h4>My Departures</h4>
 	<button on:click={loadDepartures}>Reload</button>
 </div>
+
+{#if lastRefresh}
+	<div title="{lastRefresh}">Last refresh: {lastRefreshReadable}</div>
+{/if}
 
 
 {#each monitors as monitor}
